dc6a383af017005d560e978cbee839773cf89b36
Rusil Koirala <rusilkoirala23@gmail.com>
2026-05-26 14:00:30 +0545

    Refactor main function to include server setup and graceful shutdown

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/rusilkoirala/student-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	// Load config
+	cfg := config.MustLoad()
+	// Db setup
+
+	// Router setup
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello TO NEW WORLD"))
+	})
+
+	// Setup server
+
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	fmt.Println("Server is running")
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("Failed to shutdown the server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server shutdown successfully")
 }
